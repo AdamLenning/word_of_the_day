4b094d1dab15b48d4bbbe8c9ca2b3ed918549439
adam.lenning@bengroup.com <adam.lenning@bengroup.com>
2024-07-02 17:12:51 -0600

    updated layout

--- styles.js
+++ styles.js
@@ -9,7 +9,7 @@ export default StyleSheet.create({
   },
   listContainer: {
     flex: 1,
-    marginTop: 20, // Adjust to place the list below the icons
+    marginTop: 90, // Adjust to place the list below the icons
     marginBottom: 70,
   },
   cardContainer: {
@@ -51,7 +51,7 @@ export default StyleSheet.create({
   },
   addButton: {
     position: 'absolute',
-    bottom: 20,
+    top: 60,
     right: 20,
     width: 60,
     height: 60,
@@ -62,7 +62,7 @@ export default StyleSheet.create({
   },
   settingsButton: {
     position: 'absolute',
-    bottom: 20,
+    top: 60, // Move down for iPhone notch
     left: 20,
     width: 60,
     height: 60,
